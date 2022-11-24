f19f2cd3a0fd1f8e8363013da64e99bb9079b007
morang-chun <109726121+morang-chun@users.noreply.github.com>
2022-11-24 14:25:35 +0800

    Delete test_clean.py

--- 微博爬取/test_clean.py
+++
@@ -1,8 +0,0 @@
-import openpyxl
-
-global data,sheet
-uid = "5513020355"
-
-data = openpyxl.load_workbook('E:\\PYTHON临时\\评论excel\\5513020355.xlsx') #读取格式转换后的文件三  .format(uid)
-sheet = book.create_sheet(uid)
-
